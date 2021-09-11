983b6ad417c6c899ea3afd138b67201dc1f5644f
halhal <hh.25799623@gmail.com>
2021-09-11 16:24:41 +0900

    add ReadByName and List rpc for grpc service

--- interface/grpc/handler/product_handler.go
+++ interface/grpc/handler/product_handler.go
@@ -22,7 +22,7 @@ func NewProductHandler(conn *sql.DB) *ProductHandler {
 	}
 }
 
-func (handler *ProductHandler) CreateProduct(ctx *context.Context, req *pb.CreateProductRequest) (*pb.CreateProductResponse, error) {
+func (handler *ProductHandler) CreateProduct(ctx context.Context, req *pb.CreateProductRequest) (*pb.CreateProductResponse, error) {
 	input := req.GetProductInput()
 	product := model.ProductModel{
 		Name: input.Name,
@@ -36,5 +36,43 @@ func (handler *ProductHandler) CreateProduct(ctx *context.Context, req *pb.Creat
 	} 
 	return &pb.CreateProductResponse{
 		Id: int64(id),
+	}, nil
+}
+
+func (handler *ProductHandler) ReadByNameProduct(ctx *context.Context, req *pb.ReadByNameProductRequest) (*pb.ReadByNameProductResponse, error) {
+	queryName := req.GetName()
+	var c *gin.Context
+	product, err := handler.Interactor.ProductByName(c, queryName)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.ReadByNameProductResponse{
+		Product: &pb.Product{
+			Id: int64(product.ID),
+			Name: product.Name,
+			Price: int64(product.Price),
+			UserId: int64(product.UserId),
+		},
+	}, nil
+}
+
+func (handler *ProductHandler) ListProduct(ctx *context.Context) (*pb.ListProductResponse, error) {
+	var c *gin.Context
+	products, err := handler.Interactor.Products(c)
+	if err != nil {
+		return nil, err
+	}
+	var response []*pb.Product
+	for _, v := range products {
+		product := pb.Product{
+			Id: int64(v.ID),
+			Name: v.Name,
+			Price: int64(v.Price),
+			UserId: int64(v.UserId),
+		}
+		response = append(response, &product)
+	}
+	return &pb.ListProductResponse{
+		Product: response,
 	}, nil
 }
